fed493a4adb845fae4355df35b74de6d46356dcf
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-04-01 00:07:30 +0900

    Initial commit

---
+++ 6kyu/splitStrings.js
@@ -0,0 +1,24 @@
+/*
+ *Description:
+Complete the solution so that it splits the string into pairs of two characters.
+If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+*/
+
+
+function solution(s) {
+  return (s + "_").match(/.{2}/g) || []
+}
+
+function solution(str) {
+  var i = 0;
+  var result = new Array();
+  if (str.length % 2 !== 0) {
+    str = str + '_';
+  }
+  while (i < str.length) {
+    result.push(str[i] + str[i + 1]);
+    i += 2;
+  }
+  return result;
+}
+
